98e13a6df5e5ab2450b3d1735802531e98e2a452
Piotr Proszowski <p.proszowski@gmail.com>
2021-01-10 21:11:14 +0100

    Integrate with SSO

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,3 +5,5 @@ const authBase = '/auth';
 export const signIn = async (data) => axios.post(`${authBase}/login/`, data);
 
 export const signUp = async (data) => axios.post(`${authBase}/register/`, data);
+
+export const signInWithSso = async (data) => axios.post('/sso/login/', data);
